459d310b674550907c4280a3a86430a26f666dac
1997722 <asuka.takeuchi.722@gmail.com>
2023-11-05 14:30:06 +0900

    Active Storageの導入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,10 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+  
+  private
 
+  def post_params
+    params.require(:post).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
