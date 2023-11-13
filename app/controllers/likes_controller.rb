da4ad0cd0c83476ba21e10ffa98a843cc19603b3
1997722 <asuka.takeuchi.722@gmail.com>
2023-11-14 00:16:52 +0900

    最終修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,17 +4,16 @@ class LikesController < ApplicationController
   def create
     like = current_user.likes.build(post_id: params[:post_id])
     like.save
-    redirect_to root_path
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     like.destroy
-    redirect_to root_path
+    redirect_back(fallback_location: root_path)
   end
 
   def set_post
     @post = Post.find(params[:post_id])
   end
-
 end
